c2b361960680f7619d0f2f3af57e9f3720b8b603
thompson vo <tivoisxtothecore@icloud.com>
2018-04-18 19:39:54 -0700

    Delete MainController.h

--- dumamay/vortex/MainController.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface MainController : UIViewController
-
-@property(nonatomic, strong) UINavigationController *nav;
-
-- (id)initWithNav:(UINavigationController*)nav;
-
-@end
