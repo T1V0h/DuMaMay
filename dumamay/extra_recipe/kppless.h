67441dcd1c538bdfc9b85c3f3a5769e253d1a843
thompson vo <tivoisxtothecore@icloud.com>
2018-04-18 19:24:41 -0700

    Add files via upload

---
+++ dumamay/extra_recipe/kppless.h
@@ -0,0 +1,14 @@
+//
+//  kppless.h
+//  DuMaMay
+//
+//  Created by Thompson Vo on 10/19/17.
+//  Copyright © 2017 t1_v0h!. All rights reserved.
+//
+
+#ifndef kppless_h
+#define kppless_h
+
+kern_return_t go_kppless();
+
+#endif /* kppless_h */
