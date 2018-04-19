9fe22ad254a0fbd0bb2a0c9fd6bf256eac3e9b2f
thompson vo <tivoisxtothecore@icloud.com>
2018-04-19 02:26:35 -0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "[*] Compiling DuMaMay.."
 $(which xcodebuild) clean build CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY="" -sdk `xcrun --sdk iphoneos --show-sdk-path` -arch arm64
-mv /Users/t1_v0h/Library/Developer/Xcode/DerivedData/DuMaMay-egofywexnxdkpefcdtqcqesiycqi/Build/Products/Debug-iphoneos/DuMaMay.app dumamay.app
+mv build/Release-iphoneos/DuMaMay.app dumamay.app
 mkdir Payload
 mv dumamay.app Payload/dumamay.app
 echo "[*] Zipping into .ipa"
