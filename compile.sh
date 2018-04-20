27bc4f8fd933ebd10963458988a901c8d1040be6
thompson vo <tivoisxtothecore@icloud.com>
2018-04-20 01:33:40 -0700

    Add files via upload

--- compile.sh
+++ compile.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 echo "[*] Compiling DuMaMay.."
 $(which xcodebuild) clean build CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY="" -sdk `xcrun --sdk iphoneos --show-sdk-path` -arch arm64
-mv /Build/Products/Debug-iphoneos/DuMaMay.app DuMaMay.app
+mv Downloads/DuMaMay/dumamay/Z_I_Payload/DuMaMay.app DuMaMay.app 
 mkdir Payload
 mv duMaMay.app Payload/DuMaMay.app
+cp DuMaMay.app
+PASTE Downloads/DuMaMay/dumamay/Z_I_Payload/DuMaMay.app
 echo "[*] Zipping into .ipa"
 zip -r9 DuMaMay.ipa Payload/DuMaMay.app
 rm -rf build Payload
+mv DuMaMay.ipa Desktop/DuMaMay.ipa
 echo "[*] Done! Install .ipa with Impactor"
-open 'Users/t1_v0h/'
